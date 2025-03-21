85c2138698956b9652814bdbdd53173c24c40cd6
taraskhom <khomenkotaras1305@gmail.com>
2025-03-21 16:14:46 +0200

    add model for user groups

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,6 +1,22 @@
 import enum
 
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.models.base import Base
+
+
 class UserGroupEnum(str, enum.Enum):
    USER = "user"
    MODERATOR = "moderator"
    ADMIN = "admin"
+
+class UserGroupModel(Base):
+   __tablename__ = "user_groups"
+
+
+   id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+   name: Mapped[UserGroupEnum] = mapped_column(Enum(UserGroupEnum), nullable=False, unique=True)
+
+
+   users: Mapped[list["UserModel"]] = relationship("UserModel", back_populates="group")
