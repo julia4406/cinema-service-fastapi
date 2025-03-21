a0c5df5e445ca354a04b19af69759cc27ba5d662
IvankaKuzin <kuziniwanka@gmail.com>
2025-03-21 19:45:34 +0200

    added Genre, Director and Certification models

--- src/database/models/movies.py
+++ src/database/models/movies.py
@@ -1,1 +1,37 @@
+import datetime
+from enum import Enum
+from typing import Optional, List
 
+from sqlalchemy import String, Float, Text, DECIMAL, UniqueConstraint, Date, ForeignKey, Table, Column, Integer, UUID
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import Enum as SQLAlchemyEnum
+
+from database.models.base import Base
+
+
+class StarModel(Base):
+    __tablename__ = "stars"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+
+class GenreModel(Base):
+    __tablename__ = "genres"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+
+class DirectorModel(Base):
+    __tablename__ = "directors"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+
+
+class CertificationModel(Base):
+    __tablename__ = "certifications"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
