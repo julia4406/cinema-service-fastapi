805494b79ccfabbb22a0446419d5a8e1f3e0ac97
taraskhom <khomenkotaras1305@gmail.com>
2025-03-21 16:54:51 +0200

    improve code style

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -8,68 +8,76 @@ from database.models.base import Base
 
 
 class UserGroupEnum(str, enum.Enum):
-   USER = "user"
-   MODERATOR = "moderator"
-   ADMIN = "admin"
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
 
 
 class UserGroupModel(Base):
-   __tablename__ = "user_groups"
+    __tablename__ = "user_groups"
 
-   id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-   name: Mapped[UserGroupEnum] = mapped_column(
-      Enum(UserGroupEnum), nullable=False, unique=True
-   )
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[UserGroupEnum] = mapped_column(
+        Enum(UserGroupEnum), nullable=False, unique=True
+    )
 
-   users: Mapped[list["UserModel"]] = relationship(
-      "UserModel", back_populates="group"
-   )
+    users: Mapped[list["UserModel"]] = relationship(
+        "UserModel",
+        back_populates="group",
+    )
 
 
 class UserModel(Base):
-   __tablename__ = "users"
+    __tablename__ = "users"
 
-   id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-   email: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
-   _hashed_password: Mapped[str] = mapped_column(nullable=False)
-   is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
-   created_at: Mapped[datetime] = mapped_column(
-      server_default=func.now(), nullable=False
-   )
-   updated_at: Mapped[datetime] = mapped_column(
-      server_default=func.now(), onupdate=func.now(), nullable=False
-   )
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(unique=True, nullable=False, index=True)
+    _hashed_password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
 
-   group_id: Mapped[int] = mapped_column(
-      ForeignKey("user_groups.id", ondelete="CASCADE"),
-      nullable=False
-   )
-   group: Mapped["UserGroupModel"] = relationship(
-      "UserGroupModel", back_populates="users"
-   )
+    group_id: Mapped[int] = mapped_column(
+        ForeignKey("user_groups.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    group: Mapped["UserGroupModel"] = relationship(
+        "UserGroupModel",
+        back_populates="users",
+    )
 
-   profile: Mapped["ProfileModel | None"] = relationship(
-       "UserProfileModel",
-       back_populates="user",
-       cascade="all, delete-orphan"
-   )
+    profile: Mapped["ProfileModel | None"] = relationship(
+        "UserProfileModel",
+        back_populates="user",
+        cascade="all, delete-orphan",
+    )
 
 
 class ProfileModel(Base):
-   __tablename__ = "profiles"
+    __tablename__ = "profiles"
 
-   id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-   first_name: Mapped[str | None]
-   last_name: Mapped[str | None]
-   avatar: Mapped[str | None]
-   date_of_birth: Mapped[date | None]
-   info: Mapped[str | None]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    first_name: Mapped[str | None]
+    last_name: Mapped[str | None]
+    avatar: Mapped[str | None]
+    date_of_birth: Mapped[date | None]
+    info: Mapped[str | None]
 
-   user_id: Mapped[int] = mapped_column(
-       ForeignKey("users.id", ondelete="CASCADE"),
-       nullable=False,
-       unique=True
-   )
-   user: Mapped[UserModel] = relationship("UserModel", back_populates="profile")
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True,
+    )
+    user: Mapped[UserModel] = relationship(
+        "UserModel",
+        back_populates="profile",
+    )
 
-   __table_args__ = (UniqueConstraint("user_id"),)
+    __table_args__ = (UniqueConstraint("user_id"),)
