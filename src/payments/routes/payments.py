00858b600bbcc707ffc2e8fb7e0dfc8083b094c8
Julia Alexeeva <julia4406@gmail.com>
2025-03-22 19:16:05 +0200

    Fix flake8

--- src/payments/routes/payments.py
+++ src/payments/routes/payments.py
@@ -3,14 +3,17 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+
 @router.post("/{order_id}")
 async def create_payment():
     return {"message": "Payments endpoint works!"}
+
 
 @router.get("/")
 async def get_payments_list():
     return {"message": "Payments endpoint works!"}
 
+
 @router.get("/{payment_id}")
-async def get_payments_list():
+async def get_payment_detail():
     return {"message": "Payments endpoint works!"}
