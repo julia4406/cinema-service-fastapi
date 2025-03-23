ef4862c9a31283a99bf5a17f9b13e545acc8a2f0
taraskhom <khomenkotaras1305@gmail.com>
2025-03-23 19:01:07 +0200

    add update and response schemas and validation for it

--- src/accounts/validators/__init__.py
+++ src/accounts/validators/__init__.py
@@ -1,1 +1,9 @@
-from src.accounts.validators.accounts import validate_password_strength
+from src.accounts.validators.accounts import (
+    validate_password_strength,
+    validate_info,
+    validate_avatar,
+    validate_gender,
+    validate_last_name,
+    validate_first_name,
+    validate_date_of_birth
+)
--- src/accounts/validators/accounts.py
+++ src/accounts/validators/accounts.py
@@ -1,4 +1,8 @@
+from datetime import date
 import re
+
+from database.models import GenderEnum
+
 
 def validate_password_strength(password: str) -> None:
         if len(password) < 8:
@@ -11,3 +15,44 @@ def validate_password_strength(password: str) -> None:
             raise ValueError("Password must contain at least one digit.")
         if not re.search(r'[@$!%*?&#]', password):
             raise ValueError("Password must contain at least one special character: @, $, !, %, *, ?, #, &.")
+
+def validate_first_name(value: str) -> str:
+    if not value.isalpha():
+        raise ValueError("First name must contain only letters")
+    if len(value) < 2:
+        raise ValueError("First name must be at least 2 characters long")
+    return value
+
+def validate_last_name(value: str) -> str:
+    if not value.isalpha():
+        raise ValueError("Last name must contain only letters")
+    if len(value) < 2:
+        raise ValueError("Last name must be at least 2 characters long")
+    return value
+
+def validate_gender(value: GenderEnum) -> GenderEnum:
+    allowed_genders = ["man", "woman"]
+    if value not in allowed_genders:
+        raise ValueError("Invalid gender value")
+    return value
+
+def validate_date_of_birth(value: date) -> date:
+    today = date.today()
+    if value > today:
+        raise ValueError("Date of birth cannot be in the future")
+    age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+    if age < 13:
+        raise ValueError("User must be at least 13 years old")
+    if age > 120:
+        raise ValueError("User must be at most 120 years old")
+    return value
+
+def validate_info(value: str) -> str:
+    if len(value) > 200:
+        raise ValueError("Info cannot exceed 200 characters")
+    return value
+
+def validate_avatar(value: str) -> str:
+    if not re.match(r'^https?://', value):
+        raise ValueError("Avatar must be a valid URL")
+    return value
