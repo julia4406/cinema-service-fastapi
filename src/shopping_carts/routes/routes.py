51d915741ca420a5ff666740f7120a6f9be9b170
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-03-23 17:04:46 +0200

    feat: cart routes

---
+++ src/shopping_carts/routes/routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, status
+
+from shopping_carts.controllers import (
+    get_cart,
+    create_cart,
+    add_item_to_cart,
+    remove_item_from_cart, clear_cart
+)
+
+router = APIRouter(prefix="/cart", tags=["cart"])
+
+router.get("/", status_code=status.HTTP_200_OK)(get_cart)
+router.post("/", status_code=status.HTTP_201_CREATED)(create_cart)
+router.post("/items/", status_code=status.HTTP_201_CREATED)(add_item_to_cart)
+router.delete("/items/{item_id}", status_code=status.HTTP_200_OK)(
+    remove_item_from_cart
+    )
+router.delete("/clear/", status_code=status.HTTP_200_OK)(clear_cart)
