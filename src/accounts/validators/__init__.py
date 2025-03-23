bd8c1c5b86b9a0be84d7bb1fb47a7a40e47aca5b
taraskhom <khomenkotaras1305@gmail.com>
2025-03-23 20:31:13 +0200

    delete avatar validator from init

--- src/accounts/validators/__init__.py
+++ src/accounts/validators/__init__.py
@@ -1,7 +1,6 @@
 from src.accounts.validators.accounts import (
     validate_password_strength,
     validate_info,
-    validate_avatar,
     validate_gender,
     validate_last_name,
     validate_first_name,
