c1e19283158724ac73ff6af9c1106217706390da
taraskhom <khomenkotaras1305@gmail.com>
2025-03-23 16:58:59 +0200

    add schemas for password managing requests

--- src/accounts/schemas/tokens.py
+++ src/accounts/schemas/tokens.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
+
+from accounts.validators import validate_password_strength
 
 
 class JWTTokenResponse(BaseModel):
@@ -9,5 +11,30 @@ class JWTTokenResponse(BaseModel):
     class Config:
         from_attributes = True
 
+
 class RefreshTokenRequest(BaseModel):
     refresh_token: str
+
+
+class ChangePasswordRequest(BaseModel):
+    old_password: str
+    new_password: str
+
+    @field_validator("new_password")
+    def check_password(cls, password: str):
+        validate_password_strength(password)
+        return password
+
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr
+
+
+class ResetPasswordRequest(BaseModel):
+    token: str
+    new_password: str
+
+    @field_validator("new_password")
+    def check_password(cls, password: str):
+        validate_password_strength(password)
+        return password
