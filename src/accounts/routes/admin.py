aae536a82e91e9680711e90d00ef54f91a73928f
taraskhom <khomenkotaras1305@gmail.com>
2025-03-24 18:35:41 +0200

    add routers for admin panel

---
+++ src/accounts/routes/admin.py
@@ -0,0 +1,141 @@
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+
+from src.database.session_postgresql import get_postgresql_db
+from src.database.models import UserModel
+from src.accounts.schemas.accounts import (
+    UserAdminCreateRequest,
+    UserAdminUpdateRequest,
+    ProfileUpdateRequest,
+    UserAdminResponse
+)
+from src.accounts.services.accounts import AccountsService, ProfileService
+from src.accounts.dependencies import role_required
+from src.database.models import UserGroupEnum
+
+router = APIRouter(prefix="/admin", tags=["admin"])
+
+@router.get("/users/{user_email}", response_model=UserAdminResponse)
+async def get_user_by_email(
+    user_email: EmailStr,
+    current_user: UserModel = Depends(role_required(UserGroupEnum.ADMIN)),
+    db: AsyncSession = Depends(get_postgresql_db)
+):
+    service = AccountsService(db)
+
+    try:
+        user = await service.get_by_email(user_email)
+        result = await db.execute(
+            select(UserModel)
+            .filter_by(id=user.id)
+            .options(
+                joinedload(UserModel.group),
+                joinedload(UserModel.profile)
+            )
+        )
+        user_with_data = result.scalar_one_or_none()
+        if not user_with_data:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return UserAdminResponse(
+            id=user_with_data.id,
+            email=user_with_data.email,
+            is_active=user_with_data.is_active,
+            group=UserGroupEnum(user_with_data.group.name),
+            first_name=user_with_data.profile.first_name if user_with_data.profile else None,
+            last_name=user_with_data.profile.last_name if user_with_data.profile else None,
+            avatar=user_with_data.profile.avatar if user_with_data.profile else None,
+            gender=user_with_data.profile.gender if user_with_data.profile else None,
+            date_of_birth=user_with_data.profile.date_of_birth if user_with_data.profile else None,
+            info=user_with_data.profile.info if user_with_data.profile else None
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.post("/users", response_model=UserAdminResponse)
+async def register_user(
+    user_data: UserAdminCreateRequest,
+    current_user: UserModel = Depends(role_required(UserGroupEnum.ADMIN)),
+    db: AsyncSession = Depends(get_postgresql_db)
+):
+    service = AccountsService(db)
+    try:
+        return await service.register_user_by_admin(user_data)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.patch("/users/{user_id}", response_model=UserAdminResponse)
+async def update_user(
+    user_id: int,
+    user_data: UserAdminUpdateRequest,
+    current_user: UserModel = Depends(role_required(UserGroupEnum.ADMIN)),
+    db: AsyncSession = Depends(get_postgresql_db)
+):
+    service = AccountsService(db)
+    try:
+        return await service.update_user(user_id, user_data)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.patch("/profile/{user_id}", response_model=UserAdminResponse)
+async def update_profile(
+    user_id: int,
+    profile_data: ProfileUpdateRequest,
+    current_user: UserModel = Depends(role_required(UserGroupEnum.ADMIN)),
+    db: AsyncSession = Depends(get_postgresql_db)
+):
+    profile_service = ProfileService(db)
+    account_service = AccountsService(db)
+    try:
+        user = await account_service.user_repo.get_by_id(user_id)
+        if not user:
+            raise ValueError("User not found")
+
+        await profile_service.update_profile(user, profile_data)
+
+        result = await db.execute(
+            select(UserModel)
+            .filter_by(id=user.id)
+            .options(
+                joinedload(UserModel.group),
+                joinedload(UserModel.profile)
+            )
+        )
+        user_with_data = result.scalar_one_or_none()
+        if not user_with_data:
+            raise ValueError("User not found after update")
+
+        return UserAdminResponse(
+            id=user_with_data.id,
+            email=user_with_data.email,
+            is_active=user_with_data.is_active,
+            group=UserGroupEnum(user_with_data.group.name.lower()),
+            first_name=user_with_data.profile.first_name if user_with_data.profile else None,
+            last_name=user_with_data.profile.last_name if user_with_data.profile else None,
+            avatar=user_with_data.profile.avatar if user_with_data.profile else None,
+            gender=user_with_data.profile.gender if user_with_data.profile else None,
+            date_of_birth=user_with_data.profile.date_of_birth if user_with_data.profile else None,
+            info=user_with_data.profile.info if user_with_data.profile else None
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.delete("/users/{user_id}", status_code=204)
+async def delete_user(
+    user_id: int,
+    current_user: UserModel = Depends(role_required(UserGroupEnum.ADMIN)),
+    db: AsyncSession = Depends(get_postgresql_db)
+):
+    service = AccountsService(db)
+    try:
+        await service.delete_user(user_id, current_user)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    return None
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from payments.routes.payments import router as payment_router
 from src.accounts.routes.auth import router as auth_router
 from src.accounts.routes.profile import router as profile_router
+from src.accounts.routes.admin import router as admin_router
 
 app = FastAPI(
     title="Online Cinema Service"
@@ -11,6 +12,7 @@ app = FastAPI(
 api_version_prefix = "/api/v1"
 app.include_router(auth_router)
 app.include_router(profile_router)
+app.include_router(admin_router)
 
 app.include_router(
     payment_router,
