0c554e9bb0c605aba67932b77a577455f969b9eb
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-03-25 09:57:29 +0200

    feat: exceptions for orders

---
+++ src/database/exceptions/orders.py
@@ -0,0 +1,14 @@
+class OrderBaseError(Exception):
+    pass
+
+
+class CreateOrderError(OrderBaseError):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class OrderUpdateError(OrderBaseError):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
