252344a2d13fb431245e56a47e8461a8524a08e8
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-03-25 09:56:54 +0200

    feat: orders schemas

---
+++ src/orders/schemas/orders.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+
+from database.models import StatusEnum
+
+
+class OrderItemResponseSchema(BaseModel):
+    id: int
+    movie_id: int
+    price_at_order: float
+    name: str
+    genres: Optional[List[str]] = None
+    year: int
+
+    class Config:
+        from_attributes = True
+
+
+class OrderResponseSchema(BaseModel):
+    id: int
+    user_id: int
+    created_at: datetime
+    status: StatusEnum
+    total_amount: Optional[List[str]] = None
+    items: List[OrderItemResponseSchema]
+
+    class Config:
+        from_attributes = True
+
+
+class OrderListResponseSchema(BaseModel):
+    orders: List[OrderResponseSchema]
+
+
+class MessageResponseSchema(BaseModel):
+    message: str
