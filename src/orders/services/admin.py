b8359bcca8681f55dab4b06b0b424140ed18b293
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-04-02 15:41:47 +0300

    order deps and errors

--- src/orders/services/admin.py
+++ src/orders/services/admin.py
@@ -1,10 +1,11 @@
 from typing import List, Optional, Tuple
 from datetime import datetime
+
+from shopping_carts.interfaces.repositories import AbstractCartRepository
 from src.database.models import StatusEnum
 from src.orders.dto.orders import Order
 from src.orders.interfaces.repositories import OrderRepositoryInterface
 from src.orders.interfaces.services import AdminOrderServiceInterface
-from src.shopping_carts.interfaces.repositories import CartRepositoryInterface
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.config.logging_settings import logger
 
@@ -13,7 +14,7 @@ class AdminOrderService(AdminOrderServiceInterface):
     def __init__(
             self,
             order_repository: OrderRepositoryInterface,
-            cart_repository: CartRepositoryInterface,
+            cart_repository: AbstractCartRepository,
             session: AsyncSession
     ):
         self._order_repository = order_repository
@@ -29,7 +30,9 @@ class AdminOrderService(AdminOrderServiceInterface):
             limit: int = 100,
             offset: int = 0
     ) -> Tuple[List[Order], int]:
-        logger.info(f"Fetching all orders with filters: user_id={user_id}, date_from={date_from}, date_to={date_to}, status={status}, limit={limit}, offset={offset}")
+        logger.info(
+            f"Fetching all orders with filters: user_id={user_id}, date_from={date_from}, date_to={date_to}, status={status}, limit={limit}, offset={offset}"
+        )
         orders, total = await self._order_repository.get_all_orders(
             user_id, date_from, date_to, status, limit, offset
         )
@@ -41,12 +44,16 @@ class AdminOrderService(AdminOrderServiceInterface):
             order_id: int,
             status: StatusEnum
     ) -> Order:
-        logger.info(f"Updating order status for order_id: {order_id} to {status}")
+        logger.info(
+            f"Updating order status for order_id: {order_id} to {status}"
+        )
         order = await self._order_repository.get_order_by_id(order_id)
         if not order:
             logger.warning(f"Order with order_id {order_id} not found")
             raise ValueError("Order not found")
         await self._order_repository.update_order_status(order_id, status)
         updated_order = await self._order_repository.get_order_by_id(order_id)
-        logger.info(f"Order status for order_id {order_id} updated to {status}")
+        logger.info(
+            f"Order status for order_id {order_id} updated to {status}"
+        )
         return updated_order
