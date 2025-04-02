144bce724a3b9500dabc217d8ed83a6cee68aa2a
Julia Alexeeva <julia4406@gmail.com>
2025-04-02 09:53:44 +0300

    Added logging settings

--- src/config/logging_settings.py
+++ src/config/logging_settings.py
@@ -1,7 +1,17 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_format = "%(asctime)s - %(levelname)s - %(message)s"
+formatter = logging.Formatter(log_format)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+file_handler = logging.FileHandler('cinema_api.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
