6777e8822ca8a9a1eeb869e1ce68ce25da0ad735
Julia Alexeeva <julia4406@gmail.com>
2025-04-09 20:36:59 +0300

    Flake8 and isort for celery_service

--- src/celery_service/tasks.py
+++ src/celery_service/tasks.py
@@ -10,8 +10,8 @@ from src.database.session_postgresql import get_postgresql_db
 
 
 @shared_task
-def delete_expired_activation_tokens():
-    async def async_task():
+def delete_expired_activation_tokens() -> None:
+    async def async_task() -> None:
         async for db in get_postgresql_db():
             stmt = delete(ActivationTokenModel).where(
                 ActivationTokenModel.expires_at < datetime.now(timezone.utc).replace(tzinfo=None)
@@ -26,8 +26,8 @@ def delete_expired_activation_tokens():
 
 
 @shared_task
-def delete_expired_refresh_tokens():
-    async def async_task():
+def delete_expired_refresh_tokens() -> None:
+    async def async_task() -> None:
         async for db in get_postgresql_db():
             stmt = delete(RefreshTokenModel).where(
                 RefreshTokenModel.expires_at < datetime.now(timezone.utc).replace(tzinfo=None)
@@ -42,8 +42,8 @@ def delete_expired_refresh_tokens():
 
 
 @shared_task
-def delete_expired_password_reset_tokens():
-    async def async_task():
+def delete_expired_password_reset_tokens() -> None:
+    async def async_task() -> None:
         async for db in get_postgresql_db():
             stmt = delete(PasswordResetTokenModel).where(
                 PasswordResetTokenModel.expires_at < datetime.now(timezone.utc).replace(tzinfo=None)
